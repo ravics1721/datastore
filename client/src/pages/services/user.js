ce67962ff898e87620cf692ae158e7619b749738
Ravi Bharti <ravics1718@gmail.com>
2022-09-30 00:07:40 +0530

    created api services

---
+++ client/src/pages/services/user.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE__API_URL || "http://localhost:5050/api";
+const axs = axios.create({
+  baseURL: API_URL,
+});
+
+const getUsers =
+  (page = 1) =>
+  () => {
+    return axs.get(`/user?page=${page}`);
+  };
+
+const createNewUser = (user) => {
+  return axs.post(`/user`, user);
+};
+const updateUser = ({ id, user }) => {
+  return axs.put(`/user/${id}`, user);
+};
+
+export { getUsers, createNewUser, updateUser };

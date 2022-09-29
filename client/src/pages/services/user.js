872854fbcc1bd4deb7774ebaf4815f83a97f745b
Ravi Bharti <ravics1718@gmail.com>
2022-09-30 00:22:45 +0530

    fixed api url typo

--- client/src/pages/services/user.js
+++ client/src/pages/services/user.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE__API_URL || "http://localhost:5050/api";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5050/api";
 const axs = axios.create({
   baseURL: API_URL,
 });

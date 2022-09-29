e397d8451506f9ebe9e7f4fb14a7c776a3c075b7
Ravi Bharti <ravics1718@gmail.com>
2022-09-30 00:05:14 +0530

    created the express application

--- api/index.js
+++ api/index.js
@@ -1,19 +1,34 @@
 import { config } from "dotenv";
-config()
+config();
 import express from "express";
+import morgan from "morgan";
+import router from "./api.js";
+import mongoose from "mongoose";
+import cors from "cors";
 
 const app = express();
 
+const DB_URL = process.env.DB_URL;
+
+mongoose.connect(DB_URL);
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors())
 const port = process.env.PORT || 5050;
 
 app.get("/", (req, res) => {
-    res.json({
-        message:"Hello There"
-    })
+  res.json({
+    message: "Hello There",
+  });
 });
 
+app.use("/api", router);
 
-app.listen(port, ()=>{
-    console.log(`[Server]: is running on http://localhost:${port}`);
+app.get("/*", (req, res) => {
+  res.json({ message: "NOT FOUND" });
 });
 
+app.listen(port, () => {
+  console.log(`[Server]: is running on http://localhost:${port}`);
+});
